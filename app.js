63a9f320d65991cce0497f0020d06f85307bb0af
Moisés Madeira <msalcmd@gmail.com>
2024-05-22 14:42:48 -0300

    Update app.js
    removed hardcoded link (used for testing purposes...)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.getElementById('passwordForm').addEventListener('submit', function(even
     var passwordInput = document.getElementById('password');
     var password = passwordInput.value;
     if (password.length >= 7 && password.length <= 16) {
-        window.open("https://hackathon-qa-ittalent-2024-1.s3.us-east-1.amazonaws.com/index.html?usuario_logado.html", "_blank");
+        window.open("usuario_logado.html", "_blank");
     } else {
         alert('A senha deve ter entre 8 e 16 caracteres.');
         event.preventDefault(); 
